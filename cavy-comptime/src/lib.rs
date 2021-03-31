10422e5b61568d0cdcb7adffa1e13671b9742749
mcncm <33966432+mcncm@users.noreply.github.com>
2021-03-31 01:52:01 -0400

    Basic comptime error propagation
    One (superficial, but very nice) thing to have would be the propagation
    of comptime errors to the rustc error handler. This currently requires
    nightly features and a lot of hacks. Here's a skeleton that does nothing
    but pass upward the cavyc error main messages when compiled with
    `nightly-features`.

--- cavy-comptime/src/lib.rs
+++ cavy-comptime/src/lib.rs
@@ -4,6 +4,9 @@
 //! crate, it would force a cyclic dependency between that crate and the cavy
 //! library crate.
 
+#![cfg_attr(feature = "nightly-features", feature(proc_macro_diagnostic))]
+#![cfg_attr(feature = "nightly-features", feature(proc_macro_span))]
+
 use cavy_core::{
     circuit::{Gate, Lir},
     compile,
@@ -12,6 +15,12 @@ use cavy_core::{
 };
 use proc_macro::TokenStream;
 use quote::quote;
+
+#[cfg(feature = "nightly-features")]
+mod diagnostics;
+
+#[cfg(feature = "nightly-features")]
+use proc_macro::{LineColumn, TokenTree};
 
 /// Compile Cavy code at Rust-compile-time. This is the best and easiest way to
 /// use Cavy as an embedded domain-specific language for quantum coprocessors
@@ -23,16 +32,26 @@ use quote::quote;
 pub fn cavy_comptime(input: TokenStream) -> TokenStream {
     let conf = Config::default();
     let mut ctx = Context::new(&conf);
-    let src = input.to_string();
+    let src = stringify(input);
     let id = ctx.srcs.insert_input(&src);
     let circ = compile::compile_circuit(id, &mut ctx);
     // Can only get Ok(None) if compiler options ask to stop early
     let circ = circ.map(|circ| circ.unwrap());
 
+    #[allow(unused_variables)]
     match circ {
         Ok(circ) => quote_circuit(circ),
-        Err(_) => {
-            panic!("Cavy compilation error!");
+        Err(errs) => {
+            #[cfg(not(feature = "nightly-features"))]
+            {
+                panic!("Cavy compilation error!");
+            }
+
+            #[cfg(feature = "nightly-features")]
+            {
+                diagnostics::emit_diagnostics(errs, &mut ctx);
+                (quote! { Vec::new() }).into()
+            }
         }
     }
 }
@@ -61,3 +80,14 @@ fn quote_circuit(circ: Lir) -> TokenStream {
 
     circuit_def.into()
 }
+
+#[cfg(not(feature = "nightly-features"))]
+fn stringify(input: TokenStream) -> String {
+    input.to_string()
+}
+
+#[cfg(feature = "nightly-features")]
+fn stringify(input: TokenStream) -> String {
+    input.to_string()
+}
+
