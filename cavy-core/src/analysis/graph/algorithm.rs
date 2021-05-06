fbb923da32c298d40f83be8405858d71a43ce827
mcncm <33966432+mcncm@users.noreply.github.com>
2021-05-05 21:26:54 -0400

    A new DFS algorithm that takes closures
    I can use this generically for the various DFS-related tasks in the
    analysis phase. It is also probably the worst code I've written in this
    project.

--- cavy-core/src/analysis/graph/algorithm.rs
+++ cavy-core/src/analysis/graph/algorithm.rs
@@ -46,13 +46,92 @@ where
     /// Compute pre- and post-orders. At first, we'll assume that the entire
     /// graph is reachable from the root.
     fn traversals(&self) -> (Preorder<T>, Postorder<T>) {
-        let walker = DfsWalker {
-            visited: HashSet::new(), // could optimize to BitVec
-            pre: vec![],
-            post: vec![],
-            gr: self,
+        let (mut pre, mut post) = (vec![], vec![]);
+        let pre_cb = |node| pre.push(node);
+        let post_cb = |node| post.push(node);
+
+        let mut walker = self.dfs_walker(self.root(), Box::new(pre_cb), Box::new(post_cb));
+        // Exhaust the generator
+        while walker().is_some() {}
+        drop(walker);
+        // Now our orders are full!
+        (Preorder(pre), Postorder(post))
+    }
+
+    /*
+    Ok, this was probably pretty irresponsible. I've *basically* just made a
+    struct here, with methods and everything, but in a bizarre functional style
+    full of `std::mem::replace` that no one will be able to read. This is
+    officially the worst code in this project. I'm sorry, world.
+    */
+
+    /// Get a DFS iterator starting from any node
+    fn dfs_from<'a>(&'a self, node: T) -> std::iter::FromFn<Box<dyn FnMut() -> Option<T> + 'a>>
+    where
+        T: 'a,
+    {
+        let mut walker = self.dfs_walker(node, Box::new(Option::from), Box::new(|_| None));
+        let nodes = Box::new(move || walker().flatten());
+        std::iter::from_fn(nodes)
+    }
+
+    /// Produce a DFS walker starting with a given node.
+    fn dfs_walker<'a, U>(
+        &'a self,
+        // Starting node
+        mut node: T,
+        // Callback on entering a node
+        mut pre: Box<dyn FnMut(T) -> U + 'a>,
+        // Callback on exiting a node
+        mut post: Box<dyn FnMut(T) -> U + 'a>,
+    ) -> Box<dyn FnMut() -> Option<U> + 'a>
+    where
+        T: 'a,
+        U: 'a,
+    {
+        let mut stack = vec![];
+        let mut visited = HashSet::new(); // can optimize to BitVec
+        let mut successors = [].iter();
+
+        let dfs = move || {
+            if visited.is_empty() {
+                let fst = node.clone();
+                successors = self.successors(&fst).iter();
+                visited.insert(fst.clone());
+                // "entering" callback with the first node
+                Some(pre(fst.clone()))
+            } else {
+                match successors.find(|elem| !visited.contains(elem)) {
+                    // There's an unvisited successor element
+                    Some(succ) => {
+                        // push a stack frame
+                        let new_succs = self.successors(&node).iter();
+                        let old_succs = std::mem::replace(&mut successors, new_succs);
+                        stack.push((node.clone(), old_succs));
+                        node = succ.clone();
+                        visited.insert(succ.clone());
+                        // "entering" callback with the new node
+                        Some(pre(succ.clone()))
+                    }
+                    // No unvisited successor elements: back up the stack!
+                    None => {
+                        match stack.pop() {
+                            Some((mut pred, mut old_succs)) => {
+                                // Restore the previous stack frame
+                                std::mem::swap(&mut successors, &mut old_succs);
+                                std::mem::swap(&mut node, &mut pred);
+                                // "leaving" callback with `node`
+                                Some(post(pred))
+                            }
+                            // No more nodes to find
+                            None => None,
+                        }
+                    }
+                }
+            }
         };
-        walker.traverse()
+
+        Box::new(dfs)
     }
 }
 
@@ -63,37 +142,5 @@ impl Digraph<BlockId> for Graph {
 
     fn successors(&self, node: &BlockId) -> &[BlockId] {
         self[*node].successors()
-    }
-}
-
-struct DfsWalker<'a, T, G: Digraph<T>>
-where
-    T: Clone + Hash + PartialEq + Eq,
-{
-    pre: Vec<T>,
-    post: Vec<T>,
-    visited: HashSet<T>,
-    gr: &'a G,
-}
-
-impl<'a, T, G: Digraph<T>> DfsWalker<'a, T, G>
-where
-    T: Clone + Hash + PartialEq + Eq,
-{
-    fn traverse(mut self) -> (Preorder<T>, Postorder<T>) {
-        let node = self.gr.root();
-        self.dfs(node);
-        (Preorder(self.pre), Postorder(self.post))
-    }
-
-    fn dfs(&mut self, node: T) {
-        self.visited.insert(node.clone());
-        self.pre.push(node.clone());
-        for succ in self.gr.successors(&node).iter().cloned() {
-            if !self.visited.contains(&succ) {
-                self.dfs(succ);
-            }
-        }
-        self.post.push(node);
     }
 }
